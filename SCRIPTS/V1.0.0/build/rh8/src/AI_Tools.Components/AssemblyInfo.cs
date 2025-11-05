653a8c5f122633a3b1e99fe9ef4a03dd318efb37
jmuozan <146674258+jmuozan@users.noreply.github.com>
2025-11-05 11:01:09 +0100

    Set up for version 2

---
+++ SCRIPTS/V1.0.0/build/rh8/src/AI_Tools.Components/AssemblyInfo.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Reflection;
+using System.Runtime.Versioning;
+using System.Runtime.InteropServices;
+
+using Rhino.PlugIns;
+
+[assembly: PlugInDescription(DescriptionType.Organization, "")]
+[assembly: PlugInDescription(DescriptionType.Email, "jmuozan@gmail.com")]
+[assembly: PlugInDescription(DescriptionType.Phone, "")]
+[assembly: PlugInDescription(DescriptionType.Address, "")]
+[assembly: PlugInDescription(DescriptionType.Country, "Spain")]
+[assembly: PlugInDescription(DescriptionType.WebSite, "https://jmuozan.github.io/jorgemunyozz.github.io/")]
+[assembly: PlugInDescription(DescriptionType.UpdateUrl, "https://jmuozan.github.io/jorgemunyozz.github.io/")]
+[assembly: PlugInDescription(DescriptionType.Icon, "AI_Tools.Components.Resources.projectIcon.ico")]
+
+[assembly: AssemblyTitle("AI_Tools.Components")]
+[assembly: AssemblyDescription("Generate Breps in Grasshopper with prompts (Using ZOO's Text to CAD model) or from images (Using OpenAI's vision model)")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AI_Tools")]
+[assembly: AssemblyCopyright("Copyright © 2024 Jorge Muñoz Zanón")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("11d238a3-2ff7-4f42-bf4e-da1fee9a63fa")]
+[assembly: AssemblyVersion("0.1.31215.9062")]
+[assembly: AssemblyFileVersion("0.1.31215.9062")]
+[assembly: AssemblyInformationalVersion("0.1.31215.9062")]
+
+[assembly: TargetFramework(".NETFramework,Version=v4.8", FrameworkDisplayName = ".NET Framework 4.8")]
