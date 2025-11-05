790153434b5ec3e14158e619521accd5527a87a7
jmuozan <146674258+jmuozan@users.noreply.github.com>
2025-11-05 11:21:53 +0100

    template for new plugin

---
+++ AI_3D_Models_GrasshopperInfo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Drawing;
+using Grasshopper;
+using Grasshopper.Kernel;
+
+namespace AI_3D_Models_Grasshopper
+{
+  public class AI_3D_Models_GrasshopperInfo : GH_AssemblyInfo
+  {
+    public override string Name => "AI_3D_Models_Grasshopper Info";
+
+    //Return a 24x24 pixel bitmap to represent this GHA library.
+    public override Bitmap Icon => null;
+
+    //Return a short string describing the purpose of this GHA library.
+    public override string Description => "";
+
+    public override Guid Id => new Guid("d2c5c87c-5971-4e3b-963e-055ed4b8678f");
+
+    //Return a string identifying you or your company.
+    public override string AuthorName => "";
+
+    //Return a string representing your preferred contact details.
+    public override string AuthorContact => "";
+
+    //Return a string representing the version.  This returns the same version as the assembly.
+    public override string AssemblyVersion => GetType().Assembly.GetName().Version.ToString();
+  }
+}
